ee19763bd822da961c20a249f4f64157ef9d616c
IliaNekleenov <neiluha@mail.ru>
2019-09-07 17:37:18 +0300

    delete comments, change tests

--- include/header.hpp
+++ include/header.hpp
@@ -6,43 +6,27 @@
 #include <any>
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <unordered_map>
 #include <vector>
-#include <stdexcept>
 
 class Json {
   std::any data;
 
  public:
   Json();
-  // Конструктор из строки, содержащей Json-данные.
-  Json(const std::string& s);
+  explicit Json(const std::string& s);
 
-  // Метод возвращает true, если данный экземпляр содержит в себе JSON-массив.
-  // Иначе false.
   bool is_array() const;
-  // Метод возвращает true, если данный экземпляр содержит в себе JSON-объект.
-  // Иначе false.
   bool is_object() const;
 
-  // Метод возвращает значение по ключу key, если экземпляр является
-  // JSON-объектом. Значение может иметь один из следующих типов: Json,
-  // std::string, double, bool или быть пустым. Если экземпляр является
-  // JSON-массивом, генерируется исключение.
   std::any& operator[](const std::string& key);
 
-  // Метод возвращает значение по индексу index, если экземпляр является
-  // JSON-массивом. Значение может иметь один из следующих типов: Json,
-  // std::string, double, bool или быть пустым. Если экземпляр является
-  // JSON-объектом, генерируется исключение.
   std::any& operator[](int index);
 
-  // Метод возвращает объект класса Json из строки, содержащей Json-данные.
   static Json parse(const std::string& s);
 
-  // Метод возвращает объекта класса Json из файла, содержащего Json-данные в
-  // текстовом формате.
   static Json parseFile(const std::string& path_to_file);
 
   static Json market(const std::string& s);
